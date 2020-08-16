0a4880572c3688679bfde02973a75cb779c25a58
Princiya <princiya.marina@gmail.com>
2020-08-16 10:41:44 +0200

    updated to new blog address

--- js/social.js
+++ js/social.js
@@ -6,7 +6,7 @@
 
     socialSection.innerHTML = `
   <a class="social__icon icon--twitter" href="https://twitter.com/princi_ya" target="_blank"></a>
-  <a class="social__icon icon--wordpress" href="https://princiya777.wordpress.com" target="_blank"></a>
+  <a class="social__icon icon--wordpress" href="http://princiya.com/blog" target="_blank"></a>
   <a class="social__icon icon--github" href="https://github.com/princiya" target="_blank"></a>
   <a class="social__icon icon--linkedin" href="https://www.linkedin.com/in/princiya-sequeira-0a545a34/" target="_blank"></a>
 `;
