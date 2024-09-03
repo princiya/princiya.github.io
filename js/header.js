7ef1233e8bfe0813a8116b24740001fd958703ae
Princiya Marina Sequeira <princiya.marina@gmail.com>
2024-09-03 14:28:06 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -4,7 +4,7 @@
     <a class="header__link" href="/pages/about.html">About Me</a>
     <a class="header__link" href="/pages/talks.html">Speaking</a>
     <a class="header__link" href="/pages/articles.html">Writing</a>
-    <a class="header__link" href="http://princiya.com/blog">Blog</a>
+    <a class="header__link" href="/blog">Blog</a>
     <a class="header__link" href="/pages/testimonials.html">Testimonials</a>
   `;
 })();
